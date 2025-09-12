4ef14c6517defa822e6f2978cb16944a1cd0d0df
Reece <18008512+BetaPuddi@users.noreply.github.com>
2025-09-12 19:08:42 +0100

    Tofu nerf

--- Assets/Scripts/Character/PlayerHauntedTofu.cs
+++ Assets/Scripts/Character/PlayerHauntedTofu.cs
@@ -8,7 +8,7 @@ namespace Character
     {
         public override void Attack()
         {
-            var damageOut = currentHealth * (100 - EnemyManager.instance.targetEnemy.defenseStat) / 100 * 0.5f;
+            var damageOut = currentHealth * (100 - EnemyManager.instance.targetEnemy.defenseStat) / 100 * 0.2f;
             LogManager.instance.InstantiateDamageLog(playerName, EnemyManager.instance.targetEnemy.enemyName, damageOut);
             EnemyManager.instance.targetEnemy.TakeDamage(damageOut);
         }
@@ -16,7 +16,7 @@ namespace Character
         public override void UtilitySkill_01()
         {
             var missingHealth = maxHealth - currentHealth;
-            var damageOut = missingHealth * (100 - EnemyManager.instance.targetEnemy.defenseStat) / 100;
+            var damageOut = missingHealth * (100 - EnemyManager.instance.targetEnemy.defenseStat) / 100 * 0.1f;
             LogManager.instance.InstantiateDamageLog(playerName, EnemyManager.instance.targetEnemy.enemyName, damageOut);
             LogManager.instance.InstantiateTextLog("You lose 2 Defense!");
             EnemyManager.instance.targetEnemy.TakeDamage(damageOut);
